fa1fef6830b477e98124f9ba0c96c223e9c2a59f
Bilal Shafiq <mbilalshafiq9@gmail.com>
2026-03-11 15:28:14 +0500

    small fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
 import React ,{useEffect} from 'react';
 import AppRouter from './router/index';
-import WalkthroughPopup from './components/WalkthroughPopup';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -47,7 +46,6 @@ function App() {
     <div>
       <AppRouter />      
       <ToastContainer />
-      <WalkthroughPopup />      
 
     </div>
   );
